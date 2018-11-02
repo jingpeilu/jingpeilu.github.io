5fa6a9d3e88b1a48ad4e6dc86024637f06172c3e
Jingpei Lu <lujingpei@lujingpeitekiMacBook-Air.local>
2018-11-01 21:50:47 -0700

    passcode.js

---
+++ js/passcode.js
@@ -0,0 +1,17 @@
+function passWord() {
+var testV = 1;
+var pass1 = prompt('Please Enter Password',' ');
+while (testV < 3) {
+if (!pass1) 
+history.go(-1);
+if (pass1.toLowerCase() == "asdf") {
+window.open("misc/misc.html");
+break;
+} 
+testV+=1;
+var pass1 = prompt('Access Denied - Password Incorrect, Please Try Again.','');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+history.go(-1);
+return " ";
+} 
